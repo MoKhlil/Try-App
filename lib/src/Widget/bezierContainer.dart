b64b93f8276035a39302d2c040a06658f9f5b55b
Khalil <mohammed.khlil@IW.IQ>
2020-11-08 14:14:12 +0300

    login edit style

--- lib/src/Widget/bezierContainer.dart
+++ lib/src/Widget/bezierContainer.dart
@@ -11,17 +11,17 @@ class BezierContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Transform.rotate(
-        angle: -pi / 1.3,
+        angle: -pi / 1.5,
         child: ClipPath(
-        clipper: ClipPainter(),
-        child: Container(
-          height: MediaQuery.of(context).size.height *.3,
-          width: MediaQuery.of(context).size.width,
+          clipper: ClipPainter(),
+          child: Container(
+            height: MediaQuery.of(context).size.height *.25,
+            width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                colors: [Color(0xffffd600),Color(0xfff4ff81)]
+                colors: [Color(0xffffd600),Color(0xffffd600)]
               )
             ),
         ),
